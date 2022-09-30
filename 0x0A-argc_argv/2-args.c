7a4fb1d669fee304f0642600cac2c447f74ae468
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-09-30 13:13:10 -0700

    mandatory and advanced tasks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+	return (0);
+}
