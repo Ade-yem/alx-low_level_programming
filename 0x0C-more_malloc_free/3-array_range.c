479fbb3ff33b22d0825888f9052865ebede7e00f
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-10-06 13:18:55 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,4 +9,18 @@
 
 int *array_range(int min, int max)
 {
-	
+	int *p;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * ((max - min) + 2));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		p[i] = min;
+
+	return (p);
+}
