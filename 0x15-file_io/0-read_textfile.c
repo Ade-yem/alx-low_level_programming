84f337cf617b10d92801b4fb2980e8c0c8ae2e96
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2022-10-31 14:42:43 -0700

    test checker response

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t nwr/*, nrd*/;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+	read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, letters);
+	if (nwr == -1)
+		return (0);
+	close(fd);
+	free(buf);
+	return (nwr);
+}
