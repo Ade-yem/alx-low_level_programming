88363b8d2f6154cf64b693122c6076aea0250acd
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-06 22:08:28 +0000

    initial files

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,42 @@
 #include "hash_tables.h"
 
 /**
+ * hash_arr - function to add array.
+ * @arr: the array from the table
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key
+ * Return: the array with its value set
+ */
+
+hash_node_t *hash_arr(hash_node_t **array, const char *key, const char *value)
+{
+	hash_node_t *arr;
+
+	arr = *array;
+	while (arr != NULL)
+	{
+		if (strcmp(arr->key, key) == 0)
+		{
+			free(arr->value);
+			arr->value = strdup(value);
+			return (*array);
+		}
+		arr = arr->next;
+	}
+	arr = malloc(sizeof(hash_node_t));
+
+	if (arr == NULL)
+		return (NULL);
+
+	arr->key = strdup(key);
+	arr->value = strdup(value);
+	arr->next = *array;
+	*array = arr;
+	return (*array);
+}
+
+
+/**
  * hash_table_set - function that adds an element to the hash table.
  * @ht: is the hash table you want to add or update the key/value to
  * @key: is the key. key can not be an empty string
@@ -18,47 +54,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (key == NULL || *key == '\0')
 		return (0);
 
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	result = hash_arr(&ht->array[index], key, value);
 	if (result)
 		return (1);
-	else:
+	else
 		return (0);
-
 }
-/**
- * hash_arr - function to add array.
- * @arr: the array from the table
- * @key: is the key. key can not be an empty string
- * @value: is the value associated with the key
- * Return: the array with its value set
- */
-
-hash_node_t *hash_arr(hash_node_t **array, const char *key, const char *value)
-{
-	hash_node_t *arr = *array;
-
-	strcpy(arr->key, key);
-	arr->value = value;
-
-	if (first[key] == NULL)
-    {
-       first[key] = newptr;
-    }
-    // check for insertion at tail
-    else
-    {
-        node* predptr = first[key];
-        while (true)
-        {
-            // insert at tail
-            if (predptr->next == NULL)
-            {
-                predptr->next = newptr;
-                break;
-            }
-
-            // update pointer
-            predptr = predptr->next;
-        }
-    }
