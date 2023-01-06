f0fba971de537d749e7bfe173d1d49ce6fcbe177
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-06 22:18:26 +0000

    fixed task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 
 	temp = ht->array[index];
-	while (temp != NULL )
+	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
